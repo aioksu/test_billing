005b437edd227b936d960d4be2b596854c26916c
aioksu <70708041+aioksu@users.noreply.github.com>
2020-09-04 02:00:18 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker-compose build --no-cache
 docker-compose up -d db
-docker-compose up -d rest
 sleep 10
+docker-compose up -d rest
 docker exec db bash -c "mysql -u root -ppassword -D billing < /tmp/initial.sql"
 
